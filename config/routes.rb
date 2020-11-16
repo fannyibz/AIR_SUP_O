792f79df44287736e7bbb1377bd28b7317a4c0bd
fannyibanez <fannyibanez89@gmail.com>
2020-11-16 18:14:46 +0100

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :services do
+    resources :bookings, only: [:index, :create]
+  end
+
+  resources :reviews, only: [:create]
+
+  resources :bookings, only: [:destroy]
 end
