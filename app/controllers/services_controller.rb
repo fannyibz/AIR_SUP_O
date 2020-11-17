65525553e5b5edc56ab2aab98f3642142cbde9ad
Magali Rousseau <rousseaumagali@gmail.com>
2020-11-17 15:03:15 +0100

    controller new and create

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @services = Service.all
   end
@@ -8,9 +8,19 @@ class ServicesController < ApplicationController
   end
 
   def new
+    @service = Service.new
+    authorize @service
   end
 
   def create
+    @service = Service.new(services_params)
+    @service.user = current_user
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
+    authorize @service
   end
 
   def edit
@@ -21,4 +31,13 @@ class ServicesController < ApplicationController
 
   def destroy
   end
+
+  private
+  def services_params
+    params.require(:service).permit(:name, :description, :address, photos: [])
+  end
+
+
+
+
 end
