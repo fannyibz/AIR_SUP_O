378edc42a611e21ce477431b3ba4a3253d5ef9be
Magali Rousseau <rousseaumagali@gmail.com>
2020-11-20 12:56:23 +0100

    fixed bugs

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,15 +1,27 @@
 import mapboxgl from 'mapbox-gl';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
-
   const fitMapToMarkers = (map, markers) => {
+    console.log(markers);
     if (markers.length !== 0) {
       const bounds = new mapboxgl.LngLatBounds();
       markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+      map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
     }
   };
+
+  const addMarkersToMap = (map, markers) => {
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
+        .addTo(map);
+    });
+  };
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -19,18 +31,8 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
 
-   const addMarkersToMap = (map, markers) => {
-      markers.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup) // add this
-          .addTo(map);
-      });
-    };
-    fitMapToMarkers(map, markers);
     addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
   }
 };
 
