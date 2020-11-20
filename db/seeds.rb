434c9318a18b553adadca5b73c10698b2281d6ad
fannyibanez <fannyibanez89@gmail.com>
2020-11-20 14:53:19 +0100

    seed bug 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -90,7 +90,7 @@ Service.destroy_all
 puts "> Creating heroes..."
 
 # BATMAN
-batman = User.new(username: 'Batman', password: "123456", email: "batman@gmail.com", bio:"I have a great car.")
+batman = User.new(username: 'Batman', password: "123456", email: "batman@gmail.com", bio:"Dark is my color.")
 
 file = URI.open("https://img.lemde.fr/2012/07/07/15/292.30846792291/465.35839543596/310.23363729625/688/0/60/0/ill_1729361_c0b3_201207071.0.988834193face_batman_ori.jpg")
 batman.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
@@ -99,7 +99,7 @@ batman.save!
 puts "#{batman.username} created!"
 
 # SUPERMAN
-superman = User.new(username: 'Superman', password: "123456", email: "superman@gmail.com", bio:"I have a great car.")
+superman = User.new(username: 'Superman', password: "123456", email: "superman@gmail.com", bio:"Handome man just for you.")
 
 file = URI.open("https://i.pinimg.com/originals/cb/9f/37/cb9f37eaf4205b8335d60c6cb91b0925.jpg")
 superman.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
@@ -109,7 +109,7 @@ puts "#{superman.username} created!"
 
 
 # CATWOMAN
-catwoman = User.new(username: 'Catwoman', password: "123456", email: "catwoman@gmail.com", bio:"I have a great car.")
+catwoman = User.new(username: 'Catwoman', password: "123456", email: "catwoman@gmail.com", bio:"Miaouuuuuuuuuuu.")
 
 file = URI.open("https://i.pinimg.com/originals/c7/2a/aa/c72aaa5612c6756020d746ae32fbf980.jpg")
 catwoman.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
@@ -118,7 +118,7 @@ catwoman.save!
 puts "#{catwoman.username} created!"
 
 # WONDERWOMAN
-wonderwoman = User.new(username: 'Wonderwoman', password: "123456", email: "wonderwoman@gmail.com", bio:"I have a great car.")
+wonderwoman = User.new(username: 'Wonderwoman', password: "123456", email: "wonderwoman@gmail.com", bio:"Best woman ever.")
 
 file = URI.open("https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/09/wonder-woman.jpg")
 wonderwoman.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
@@ -127,7 +127,7 @@ wonderwoman.save!
 puts "#{wonderwoman.username} created!"
 
 # AQUAMAN
-aquaman = User.new(username: 'Aquaman', password: "123456", email: "aquaman@gmail.com", bio:"I have a great car.")
+aquaman = User.new(username: 'Aquaman', password: "123456", email: "aquaman@gmail.com", bio:"I swime like no one.")
 
 file = URI.open("https://media.melty.fr/article-3963641-ratio15_720-f5/media.jpg")
 aquaman.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
@@ -135,50 +135,61 @@ aquaman.save!
 
 puts "#{aquaman.username} created!"
 
+# super_paper
+super_paper = User.new(username: 'Super Paper', password: "123456", email: "superpaper@gmail.com", bio:"I create wonderfull toilet paper!!")
+
+file = URI.open("https://media.melty.fr/article-3963641-ratio15_720-f5/media.jpg")
+super_paper.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
+super_paper.save!
+
+puts "#{super_paper.username} created!"
+
 # ------------- SERVICES -------------
 
-puts "> Creating services..."
+# puts "> Creating services..."
 
-photos = [
-  "https://guyhepner.com/wp-content/uploads/2015/06/081-It-Prickles.jpg",
-  "https://creativepool.com/marketing/images/secret-life-heroes-2.jpg",
-  "https://data.whicdn.com/images/301772591/original.jpg",
-  "https://i.pinimg.com/564x/36/65/a5/3665a577c51c0a370c18bf7c9b8a26c1.jpg",
-  "https://i0.wp.com/thaeger.com/wp-content/uploads/2014/07/The-Secret-Life-of-Heroes-Poison-Ivy.jpg",
-  "https://i1.wp.com/thaeger.com/wp-content/uploads/2014/07/The-Secret-Life-of-Heroes-catwoman.jpg",
-  "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(20).jpg",
-  "https://i.pinimg.com/originals/dd/a7/a1/dda7a11c57f0e0b08e00ccc451cbafb1.jpg",
-  "https://frikilogia.com/wp-content/uploads/2013/02/popartcomic9.jpg",
-  "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(31).jpg",
-  "https://cultureposters.com/wp-content/uploads/2019/12/secret-life-heroes-27.jpg",
-  "https://cultureposters.com/wp-content/uploads/2019/12/secret-life-heroes-26.jpg",
-  "https://alessioangiolini.files.wordpress.com/2014/02/1a1bfbe0fd6188a87f6bd0815e2ced6c.jpg",
-  "https://i.pinimg.com/originals/9e/79/a1/9e79a127aee66ec1579b1442a52ff165.png",
-  "https://mir-s3-cdn-cf.behance.net/project_modules/disp/df30756678291.5634518b4fdef.jpg",
-  "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(4).jpg",
-  "https://creativepool.com/marketing/images/secret-life-heroes-33.jpg",
-  "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(44).jpg",
-  "https://cultureposters.com/wp-content/uploads/2019/12/secret-life-heroes-46.jpg"
-]
+# photos = [
+#   "https://guyhepner.com/wp-content/uploads/2015/06/081-It-Prickles.jpg",
+#   "https://creativepool.com/marketing/images/secret-life-heroes-2.jpg",
+#   "https://data.whicdn.com/images/301772591/original.jpg",
+#   "https://i.pinimg.com/564x/36/65/a5/3665a577c51c0a370c18bf7c9b8a26c1.jpg",
+#   "https://i0.wp.com/thaeger.com/wp-content/uploads/2014/07/The-Secret-Life-of-Heroes-Poison-Ivy.jpg",
+#   "https://i1.wp.com/thaeger.com/wp-content/uploads/2014/07/The-Secret-Life-of-Heroes-catwoman.jpg",
+#   "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(20).jpg",
+#   "https://i.pinimg.com/originals/dd/a7/a1/dda7a11c57f0e0b08e00ccc451cbafb1.jpg",
+#   "https://frikilogia.com/wp-content/uploads/2013/02/popartcomic9.jpg",
+#   "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(31).jpg",
+#   "https://cultureposters.com/wp-content/uploads/2019/12/secret-life-heroes-27.jpg",
+#   "https://cultureposters.com/wp-content/uploads/2019/12/secret-life-heroes-26.jpg",
+#   "https://alessioangiolini.files.wordpress.com/2014/02/1a1bfbe0fd6188a87f6bd0815e2ced6c.jpg",
+#   "https://i.pinimg.com/originals/9e/79/a1/9e79a127aee66ec1579b1442a52ff165.png",
+#   "https://mir-s3-cdn-cf.behance.net/project_modules/disp/df30756678291.5634518b4fdef.jpg",
+#   "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(4).jpg",
+#   "https://creativepool.com/marketing/images/secret-life-heroes-33.jpg",
+#   "https://assets.awwwards.com/awards/images/2013/06/secret-life-heroes%20(44).jpg",
+#   "https://cultureposters.com/wp-content/uploads/2019/12/secret-life-heroes-46.jpg"
+# ]
 
-# SUPER RESCUE (BATMAN)
-super_rescue_1 = Service.new(name: 'Super Rescue', description: "To book if you are in danger!", address: "16 villa Gaudelet, Paris", user_id: batman.id, price: 299)
-super_kicker = Service.new(name: 'Super Kicker', description: "To book if you want I kick some asses!", address: "12 rue Montgrand, Marseille", user_id: wonderwoman.id, price: 59)
-super_uber = Service.new(name: 'Super Uber', description: "To book if you want a cool and badass driver!", address: "17 rue de la République, Marseille", user_id: batman.id, price: 39)
-super_escort_1 = Service.new(name: 'Super Escort', description: "To book if you are single (or not)!", address: "109 rue Saint-Lazare, Paris", user_id: aquaman.id, price: 99)
-super_rescue_2 = Service.new(name: 'Super Rescue', description: "To book if you are in danger!", address: "Place du Dome Centre commercial les 4 Temps, Puteaux", user_id: wonderwoman.id, price: 320)
-super_swimming = Service.new(name: 'Super Swimming', description: "To book if you want to know swimming!", address: "78 Avenue des Champs-Elysées, Paris", user_id: aquaman.id, price: 29)
 
-[super_rescue_1, super_kicker, super_uber].each do |hero|
-  service_photos = photos.sample(3).map do |photo|
-    URI.open(photo)
-  end
-  service_photos.each do |photo|
-    hero.photos.attach(io: photo, filename: "service", content_type: "image/jpg")
-  end
-  hero.save!
-  puts "#{hero.name} created!"
-end
+
+# # SUPER RESCUE (BATMAN)
+# super_rescue_1 = Service.new(name: 'Super Rescue', description: "To book if you are in danger!", address: "16 villa Gaudelet, Paris", user_id: batman.id, price: 299)
+# super_kicker = Service.new(name: 'Super Kicker', description: "To book if you want I kick some asses!", address: "12 rue Montgrand, Marseille", user_id: wonderwoman.id, price: 59)
+# super_uber = Service.new(name: 'Super Uber', description: "To book if you want a cool and badass driver!", address: "17 rue de la République, Marseille", user_id: batman.id, price: 39)
+# super_escort_1 = Service.new(name: 'Super Escort', description: "To book if you are single (or not)!", address: "109 rue Saint-Lazare, Paris", user_id: aquaman.id, price: 99)
+# super_rescue_2 = Service.new(name: 'Super Rescue', description: "To book if you are in danger!", address: "Place du Dome Centre commercial les 4 Temps, Puteaux", user_id: wonderwoman.id, price: 320)
+# super_swimming = Service.new(name: 'Super Swimming', description: "To book if you want to know swimming!", address: "78 Avenue des Champs-Elysées, Paris", user_id: aquaman.id, price: 29)
+
+# [super_rescue_1, super_kicker, super_uber].each do |hero|
+#   service_photos = photos.sample(3).map do |photo|
+#     URI.open(photo)
+#   end
+#   service_photos.each do |photo|
+#     hero.photos.attach(io: photo, filename: "service", content_type: "image/jpg")
+#   end
+#   hero.save!
+#   puts "#{hero.name} created!"
+# end
 
 
 
