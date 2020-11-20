0880f01970536c382fcf9ff5c912349cf8cad6d0
raphael allard <raphael.s.allard@gmail.com>
2020-11-20 12:01:16 +0100

    dashbord fixed + flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -5,8 +5,11 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 
+const initFlatPickr = () => {
+  flatpickr("#range_start", {
+    altInput: true,
+    plugins: [new rangePlugin({ input: "#range_end"})]
+  });
+};
 
-flatpickr("#range_start", {
-  altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})]
-});
+export { initFlatPickr };
